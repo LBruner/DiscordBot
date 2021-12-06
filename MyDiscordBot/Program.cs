9f453911b5cbb592cfb7f9576b01b4413f9b1bfb
LBruner <leandrobruner15@gmail.com>
2021-12-05 21:02:27 -0300

    add bot online (01)

---
+++ MyDiscordBot/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyDiscordBot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
